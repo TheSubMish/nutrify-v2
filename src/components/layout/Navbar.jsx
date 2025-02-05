67d210056db6d8409809c54f291f65d4a6753e42
submish <subodhmishra015@gmail.com>
2025-02-05 21:39:29 +0545

    fix: navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -31,7 +31,7 @@ function Navbar() {
             height={48}
             className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
           />
-          <span className="hidden sm:inline">Nutrifyme</span>
+          <span>Nutrifyme</span>
         </Link>
 
         {/* Hamburger Menu */}
